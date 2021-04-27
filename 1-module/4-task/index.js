1c41b3a2720b076d5514c856e1b3c4f429fc9192
OliviaTa <ir.klypina@gmail.com>
2021-04-27 11:52:29 +0800

    spam checking function

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  let spamStrings = ['1xbet', 'xxx'];
+
+  str = str.toLowerCase();
+
+  for (let spamStr of spamStrings) {
+    if (str.indexOf(spamStr) !== -1) return true;
+  }
+
+  return false;
 }
