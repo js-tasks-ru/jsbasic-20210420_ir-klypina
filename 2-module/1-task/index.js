6f841db708050463b89235d8c96761146b2eeb70
OliviaTa <ir.klypina@gmail.com>
2021-04-30 15:00:27 +0800

    2nd module's tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let salary = 0;
+
+  for (const key in salaries) {
+    const value = salaries[key];
+
+    if (typeof value === 'number' && isFinite(value)) {
+      salary += value;
+    }
+  }
+
+  return salary;
 }
