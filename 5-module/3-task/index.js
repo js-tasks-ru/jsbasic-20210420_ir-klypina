9a488bbf85ac2227b2485d28774c2c1a6bdff0b9
OliviaTa <ir.klypina@gmail.com>
2021-05-13 15:48:27 +0800

    5 module tasks

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,26 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel');
+  const arrowRight = carousel.querySelector('.carousel__arrow_right');
+  const arrowLeft = carousel.querySelector('.carousel__arrow_left');
+  const carouselInner = carousel.querySelector('.carousel__inner');
+
+  const slidesCount = carouselInner.querySelectorAll('.carousel__slide').length;
+  const slideWidth = carouselInner.querySelector('.carousel__slide').offsetWidth;
+  let currentSlide = 0;
+
+  const toggleVisibleArrow = () => {
+    arrowRight.style.display = currentSlide === slidesCount - 1 ? 'none' : '';
+    arrowLeft.style.display = currentSlide === 0 ? 'none' : '';
+  };
+
+  toggleVisibleArrow();
+
+  carousel.addEventListener('click', (event) => {
+    const arrow = event.target.closest('.carousel__arrow');
+    if (!arrow) return;
+
+    currentSlide = arrow.classList.contains('carousel__arrow_right') ? ++currentSlide : --currentSlide;
+    carouselInner.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+    toggleVisibleArrow();
+  });
 }
